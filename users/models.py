d5b510deff4b724822fa2ecdfe384f5919bca658
Groompel <kzdoandos@gmail.com>
2020-04-15 22:03:33 +0600

    Create user model

--- users/models.py
+++ users/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+
+class User(models.Model):
+    name = models.CharField(max_length=30)
+    username = models.CharField(max_length=20)
+    password = models.CharField(max_length=40)
+    telegram_chat_id = models.CharField(max_length=100)
+    telegram_username = models.CharField(max_length=100)
+    profile_photo = models.CharField(max_length=400)
+    faculty = models.CharField(max_length=50)
+    gender = models.CharField(max_length=50)
+    year_of_study = models.IntegerField()
